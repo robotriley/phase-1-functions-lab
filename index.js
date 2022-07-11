ec43d00241f898ac675699a7ed3976cacaa44b87
riley soloner <rileysoloner@gmail.com>
2022-07-11 09:59:44 -0400

    commit tree 060be1e8afc9b3342a227519bb056e3cb4a06ccb

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+let hq = 42
+const distanceFromHqInBlocks = (pickupLocation) => {
+    if (pickupLocation >= hq) {
+        return (pickupLocation - hq)
+    } else {
+        return (hq - pickupLocation)
+    }
+}
+const distanceFromHqInFeet = (pickupLocation) => {
+    return (distanceFromHqInBlocks(pickupLocation) * 264)
+}
+
+const distanceTravelledInFeet = (start, destination) => {
+    if (destination >= start) {
+    return ((destination - start) * 264)
+} else {
+        return ((start - destination) * 264)
+}}
+
+const calculatesFarePrice = (start, destination) => {
+    let feet = distanceTravelledInFeet(start, destination)
+    if (feet <= 400) {
+        return 0
+    } else if (feet <= 2000) {
+    return ((feet - 400) * .02)
+    } else if (feet < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+}
